c68c05344c1044fa46753bf4256107bf5a9d8907
Ritesh Pandey <riteshpandey72060@gmail.com>
2024-05-27 17:48:52 +0530

    sector perfomacne

--- src/conponent/List.jsx
+++ src/conponent/List.jsx
@@ -46,29 +46,24 @@ const MarketList = () => {
 
   return (
     <>
-      <p className="text-gray-600 px-6 py-4 font-extrabold text-xl"> Sector Performance</p>
-      <div className="market-list p-6 text-white text-sm flex justify-between w-full flex-col lg:flex-row">
-        
-        <div className="market-items flex flex-col items-center justify-between lg:w-1/2 md:flex-col ">
+      <p className="text-gray-600 px-6 py-4 font-extrabold text-xl">Sector Performance</p>
+      <div className="market-list p-6 text-white text-sm flex flex-col lg:flex-row justify-between">
+        <div className="market-items flex flex-col items-center justify-between lg:w-1/2">
           {leftSideData.map((item, index) => (
             <div key={index} className="market-item p-2 w-full max-w-md flex justify-between items-center">
               <span className="font-medium">{item.name}</span>
               <div className={`p-2 rounded-lg text-white ${item.performance >= 0 ? "bg-gradient-to-r from-gray-600 to-green-400" : "bg-gradient-to-r from-gray-600 to-red-400"}`}>
-                <span>
-                  {item.performance}
-                </span>
+                <span>{item.performance}</span>
               </div>
             </div>
           ))}
         </div>
-        <div className="market-items flex flex-col items-center justify-between lg:w-1/2 md:flex-col">
+        <div className="market-items flex flex-col items-center justify-between lg:w-1/2">
           {rightSideData.map((item, index) => (
             <div key={index} className="market-item p-2 w-full max-w-md flex justify-between items-center">
               <span className="font-medium">{item.name}</span>
               <div className={`p-2 rounded-lg text-white ${item.performance >= 0 ? "bg-gradient-to-r from-gray-600 to-green-400" : "bg-gradient-to-r from-gray-600 to-red-400"}`}>
-                <span>
-                  {item.performance}
-                </span>
+                <span>{item.performance}</span>
               </div>
             </div>
           ))}
